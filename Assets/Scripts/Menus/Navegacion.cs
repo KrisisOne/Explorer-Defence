178e41e9275763a41fad8ffdd6035a3898a3d9a2
KrisisOne <clvazquez@uoc.edu>
2024-12-02 17:12:56 +0100

    Segundo commit, cambio de apariencia de los menús. Se añade menú de selección de nivel.

--- Assets/Scripts/Menus/Navegacion.cs
+++ Assets/Scripts/Menus/Navegacion.cs
@@ -10,7 +10,37 @@ public class Navegacion : MonoBehaviour {
     }
     public void IrAModoHistoria(){
 
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Seleccion_nivel");
+
+    }
+
+    public void IrANivel1(){
+
+        SceneManager.LoadScene("Nivel1");
+
+    }
+
+    public void IrANivel2(){
+
+        SceneManager.LoadScene("Nivel2");
+
+    }
+
+    public void IrANivel3(){
+
+        SceneManager.LoadScene("Nivel3");
+
+    }
+
+    public void IrANivel4(){
+
+        SceneManager.LoadScene("Nivel4");
+
+    }
+
+    public void IrANivel5(){
+
+        SceneManager.LoadScene("Nivel5");
 
     }
 
